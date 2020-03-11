c4fbbd44db801cb144f796ff3371886f03b6f19f
Damian <piasecki9712@gmail.com>
2020-03-11 20:02:12 +0100

    Initial commit

---
+++ src/main/java/pl/my/programisci/eduapp/EduAppApplication.java
@@ -0,0 +1,13 @@
+package pl.my.programisci.eduapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EduAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EduAppApplication.class, args);
+	}
+
+}
---
+++ src/test/java/pl/my/programisci/eduapp/EduAppApplicationTests.java
@@ -0,0 +1,13 @@
+package pl.my.programisci.eduapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EduAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
